d09e3ae7de26e093fe9ee968310a00db4ca45c75
adrinospy <amanmoon096@gmail.com>
2024-12-22 05:14:38 +0530

    added routes and updated packages

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-import connectDB from "./db.connect";
 
+import connectDB from "./db.connect";
+import router from "./router"
+import webSocket from "./meet/ws.api"
 const app = express();
 
 // MIddlewares
@@ -24,9 +26,16 @@ const PORT = 8080;
 const URL =
   "mongodb+srv://user:user@meteordb.3mmiy63.mongodb.net/?retryWrites=false&w=majority";
 
+// webSocket
+webSocket();
+
 server.listen(PORT, () => {
   console.log(`METEOR running on server http://localhost:${PORT}/`);
 });
 
 // connect to mongoDB database
 connectDB(URL);
+
+app.use('/', router())
+
+export { server };
